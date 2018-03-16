f30d47299d8e607f5e03a5cdd018bb9c9b0b34a0
Alexander Atherton <athertona@cardiff.ac.uk>
2018-03-16 17:37:23 +0000

    Added sharedPreferences for default settings page

--- app/src/main/java/com/nsa/clientproject/welshpharmacy/DefaultSettings.java
+++ app/src/main/java/com/nsa/clientproject/welshpharmacy/DefaultSettings.java
@@ -1,14 +1,22 @@
 package com.nsa.clientproject.welshpharmacy;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatCheckBox;
+import android.view.View;
+import android.widget.Toast;
 
-public class DefaultSettings extends AppCompatActivity {
+public class DefaultSettings extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener, SharedPreferences.OnSharedPreferenceChangeListener {
 
-    private AppCompatCheckBox cbBP;
-    private AppCompatCheckBox cbFS;
+
+    private AppCompatCheckBox bloodPressure;
+    private AppCompatCheckBox fluShot;
+    private AppCompatButton saveButton;
+
+    private SharedPreferences sharedPreferences;
+
 
 
     @Override
@@ -16,15 +24,71 @@ public class DefaultSettings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_default_settings);
 
-        initViews();
+        //fetches widgets
 
+        this.bloodPressure = this.findViewById(R.id.cbBP);
+        this.fluShot = this.findViewById(R.id.cbFS);
+        this.saveButton = this.findViewById(R.id.savebutton);
+
+        //initialises sharededPreferences object
+        this.sharedPreferences = this.getPreferences(MODE_PRIVATE);
+
+        // method used to set the current state of widgets in sharedPreferences
+        initValues();
+
+        this.saveButton.setOnClickListener(this);
+        this.saveButton.setOnLongClickListener(this);
     }
 
-    private void initViews() {
-        cbBP = (AppCompatCheckBox) findViewById(R.id.cbBP);
-        cbFS = (AppCompatCheckBox) findViewById(R.id.cbFS);
 
+    private void initValues() {
+        if (this.sharedPreferences !=null) {
+            this.bloodPressure.setChecked(sharedPreferences.getBoolean(KeyValueHelper.KEY_BLOODPRESSURE_CHECKBOX, KeyValueHelper.DEFAULT_BLOODPRESSURE_CHECKBOX));
+            this.fluShot.setChecked(sharedPreferences.getBoolean(KeyValueHelper.KEY_FLUSHOT_CHECKBOX, KeyValueHelper.DEFAULT_FLUSHOT_CHECKBOX));
+        }
     }
 
-    public void
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (this.sharedPreferences !=null)
+            this.sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (this.sharedPreferences != null)
+            this.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        if (id == R.id.savebutton && this.sharedPreferences != null) {
+            SharedPreferences.Editor editor = this.sharedPreferences.edit();
+            editor.putBoolean(KeyValueHelper.KEY_BLOODPRESSURE_CHECKBOX, this.bloodPressure.isChecked());
+            editor.putBoolean(KeyValueHelper.KEY_FLUSHOT_CHECKBOX, this.fluShot.isChecked());
+
+            editor.apply();
+        }
+    }
+
+    @Override
+    public boolean onLongClick(View view) {
+        int id = view.getId();
+
+        if (id == R.id.savebutton && this.sharedPreferences != null) {
+            this.sharedPreferences.edit().clear().commit();
+            Toast.makeText(this, getString(R.string.reset_text), Toast.LENGTH_SHORT).show();
+            initValues();
+        }
+        return true;
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        Toast.makeText(this, R.string.saved_message, Toast.LENGTH_SHORT).show();
+    }
 }
