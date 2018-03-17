900a3bd2ec29894c27319b3d2bd04f27b8243504
Hristo Dimitrov <dimitrovhd@cardiff.ac.uk>
2018-03-17 19:38:24 +0000

    Made app flow correctly.

--- app/src/main/java/com/nsa/clientproject/welshpharmacy/PharmacyView.java
+++ app/src/main/java/com/nsa/clientproject/welshpharmacy/PharmacyView.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.ListView;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,27 +16,25 @@ import com.nsa.clientproject.welshpharmacy.models.PharmacyList;
 
 
 public class PharmacyView extends AppCompatActivity {
-    ListView lv ;
+    ListView lv;
     String[] servicesConvert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pharmacy_view);
-        PharmacyList pharmacyList = new PharmacyList();
-        pharmacyList.updatePharmacies();
-        Pharmacy pharmacy = pharmacyList.getPharmacies().get(0);
+        Pharmacy pharmacy = (Pharmacy) getIntent().getSerializableExtra("pharmacy");
 
         //Change Text in name text field in single pharmacy view
-        TextView singleName=(TextView)findViewById(R.id.single_name);
+        TextView singleName = (TextView) findViewById(R.id.single_name);
         singleName.setText(pharmacy.getName());
 
         //Change Text in times text field in single pharmacy view
-        TextView singleTimes=(TextView)findViewById(R.id.single_times);
+        TextView singleTimes = (TextView) findViewById(R.id.single_times);
         singleTimes.setText(pharmacy.getOpenTime() + " until " + pharmacy.getCloseTime());
 
         //Change Text in location text field in single pharmacy view
-        TextView singleLocation=(TextView)findViewById(R.id.single_location);
+        TextView singleLocation = (TextView) findViewById(R.id.single_location);
         singleLocation.setText("PostCode:" + pharmacy.getLocation());
 
         //Change items in services list view in single pharmacy view
