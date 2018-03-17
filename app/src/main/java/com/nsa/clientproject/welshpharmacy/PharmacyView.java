ccaadbeaa2162b155130dc095fa51913916c7bc1
Mona Lisa <WinserJM@cardiff.ac.uk>
2018-03-17 15:11:49 +0000

    Story5

--- app/src/main/java/com/nsa/clientproject/welshpharmacy/PharmacyView.java
+++ app/src/main/java/com/nsa/clientproject/welshpharmacy/PharmacyView.java
@@ -3,6 +3,8 @@ package com.nsa.clientproject.welshpharmacy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
 
 import com.nsa.clientproject.welshpharmacy.models.Pharmacy;
 import com.nsa.clientproject.welshpharmacy.models.PharmacyList;
@@ -23,6 +25,19 @@ public class PharmacyView extends AppCompatActivity {
 
         //Change Text in location text field in single pharmacy view
         TextView singleLocation=(TextView)findViewById(R.id.single_location);
-        singleLocation.setText(pharmacy.getLocation());
+        singleLocation.setText("PostCode:" + pharmacy.getLocation());
+
+        //Change items in services list view in single pharmacy view
+        setContentView(R.layout.pharmacy_view);
+
+        // Get a handle to the list view
+        ListView lv =(ListView)findViewById(R.id.single_services);
+
+        // Convert ArrayList to array
+        lv_arr = (String[]) arrayList.toArray();
+        lv.setAdapter(new ArrayAdapter<String>(Start.this,
+                android.R.layout.simple_list_item_1, lv_arr));
+
+
     }
 }
