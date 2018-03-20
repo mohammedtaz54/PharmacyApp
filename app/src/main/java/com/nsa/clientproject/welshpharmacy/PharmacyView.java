95a53451630ccc69d864fa4511d65045e6990288
c1632782 <winserjm@cardiff.ac.uk>
2018-03-20 11:49:16 +0000

    Added pharmacy services to pharmacy view.

--- app/src/main/java/com/nsa/clientproject/welshpharmacy/PharmacyView.java
+++ app/src/main/java/com/nsa/clientproject/welshpharmacy/PharmacyView.java
@@ -6,13 +6,17 @@ import android.widget.TextView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
+import android.widget.AdapterView;
+import android.view.View.OnClickListener;
+
 
 import com.nsa.clientproject.welshpharmacy.models.Pharmacy;
 import com.nsa.clientproject.welshpharmacy.models.PharmacyList;
+import com.nsa.clientproject.welshpharmacy.models.PharmacyServices;
 
 
 public class PharmacyView extends AppCompatActivity {
@@ -22,6 +26,7 @@ public class PharmacyView extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.pharmacy_view);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Pharmacy pharmacy = (Pharmacy) getIntent().getSerializableExtra("pharmacy");
@@ -32,13 +37,36 @@ public class PharmacyView extends AppCompatActivity {
 
         //Change Text in times text field in single pharmacy view
         TextView singleTimes = (TextView) findViewById(R.id.single_times);
-        singleTimes.setText(getString(R.string.opening_closing_time,pharmacy.getOpenTime(),pharmacy.getCloseTime()));
+        singleTimes.setText(getString(R.string.opening_closing_time, pharmacy.getOpenTime(), pharmacy.getCloseTime()));
 
         //Change Text in location text field in single pharmacy view
         TextView singleLocation = (TextView) findViewById(R.id.single_location);
-        singleLocation.setText(getString(R.string.address,pharmacy.getLocation()));
+        singleLocation.setText(getString(R.string.address, pharmacy.getLocation()));
 
         //Change items in services list view in single pharmacy view
+        ArrayList<String> services;
+        setContentView(R.layout.pharmacy_view);
+
+        //Reference: http://www.learn-android-easily.com/2013/05/populating-listview-with-arraylist.html
+        //accessed 20 March 2018
+        // Get the reference of ListView
+        ListView serviceList = (ListView) findViewById(R.id.single_services);
+
+
+        services = new ArrayList<String>();
+
+        for(PharmacyServices pharmacyService: pharmacy.getServicesOffered()){
+            services.add(pharmacyService.name());
+        }
+
+        // Create The Adapter with passing ArrayList as 3rd parameter
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, services);
+        // Set The Adapter
+        serviceList.setAdapter(arrayAdapter);
+
+        // register onClickListener to handle click events on each item
+        ;
 
 
     }
