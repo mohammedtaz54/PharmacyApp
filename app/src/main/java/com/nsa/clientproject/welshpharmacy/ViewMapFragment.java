417cb4f4a07200e104911c2003e1ba31403f89c5
Hristo Dimitrov <dimitrovhd@cardiff.ac.uk>
2018-03-27 10:44:21 +0100

    Added map options - if given location permission, it goes there, if not and given a postcode - goes there - if none of the above - shows the entirety of Wales.

--- app/src/main/java/com/nsa/clientproject/welshpharmacy/ViewMapFragment.java
+++ app/src/main/java/com/nsa/clientproject/welshpharmacy/ViewMapFragment.java
@@ -1,7 +1,13 @@
 package com.nsa.clientproject.welshpharmacy;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
@@ -11,6 +17,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -18,16 +26,23 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.nsa.clientproject.welshpharmacy.models.Pharmacy;
 import com.nsa.clientproject.welshpharmacy.models.PharmacyList;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 
-public class ViewMapFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+public class ViewMapFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, OnCompleteListener {
 
     private GoogleMap mMap;
     private List<Pharmacy> pharmacyList;
+    private static final int DEFAULT_ZOOM_LVL = 15;
+    private static final int WALES_ZOOM_LVL = 7;
+    private SharedPreferences defaultSettings;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -39,6 +54,7 @@ public class ViewMapFragment extends Fragment implements OnMapReadyCallback, Goo
         PharmacyList pl = new PharmacyList();
         pl.updatePharmacies();
         pharmacyList = pl.getPharmacies();
+        defaultSettings = this.getContext().getSharedPreferences("DEFAULT_SETTINGS", Context.MODE_PRIVATE);
         return v;
 
     }
@@ -46,22 +62,27 @@ public class ViewMapFragment extends Fragment implements OnMapReadyCallback, Goo
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        if(ContextCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-                == PermissionChecker.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                == PermissionChecker.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
-        }
-        else{
+            //Reference: https://developers.google.com/maps/documentation/android-api/current-place-tutorial
+            //Accessed 27 March 2018
+            FusedLocationProviderClient mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this.getContext());
+            Task locationResult = mFusedLocationProviderClient.getLastLocation();
+            locationResult.addOnCompleteListener(this);
+
+        } else {
             Toast.makeText(this.getContext(), R.string.location_not_found, Toast.LENGTH_SHORT).show();
+            moveCameraToHome();
+
         }
-        for(Pharmacy p: pharmacyList){
+        for (Pharmacy p : pharmacyList) {
             MarkerOptions marker = new MarkerOptions()
-                    .position(new LatLng(p.getPharmacyLat(),p.getPharmacyLng()))
+                    .position(new LatLng(p.getPharmacyLat(), p.getPharmacyLng()))
                     .title(p.getName());
 
             Marker m = mMap.addMarker(marker);
             m.setTag(p);
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(p.getPharmacyLat(),p.getPharmacyLng())));
-            mMap.moveCamera(CameraUpdateFactory.zoomTo(13));
         }
         mMap.setOnMarkerClickListener(this);
     }
@@ -102,10 +123,78 @@ public class ViewMapFragment extends Fragment implements OnMapReadyCallback, Goo
 
     @Override
     public boolean onMarkerClick(Marker marker) {
-        Intent i = new Intent(this.getContext(),PharmacyView.class);
-        i.putExtra("pharmacy",(Serializable)marker.getTag());
+        Intent i = new Intent(this.getContext(), PharmacyView.class);
+        i.putExtra("pharmacy", (Serializable) marker.getTag());
         startActivity(i);
         return true;
+    }
+
+    /**
+     * Called when the user's location is found
+     */
+    @Override
+    public void onComplete(@NonNull Task task) {
+        if (task.isSuccessful()) {
+            moveCameraToLocation((Location) task.getResult(), DEFAULT_ZOOM_LVL);
+        } else {
+            moveCameraToHome();
+        }
+    }
+
+    private void moveCameraToHome() {
+        String postcode = defaultSettings.getString(KeyValueHelper.KEY_POSTCODE_TEXT, KeyValueHelper.DEFAULT_POSTCODE_TEXT);
+        if (!postcode.equals(KeyValueHelper.DEFAULT_POSTCODE_TEXT)) {
+            Geocoder geocoder = new Geocoder(this.getContext());
+            try {
+                Address address = geocoder.getFromLocationName(postcode, 1).get(0);
+                moveCameraToLocation(address, DEFAULT_ZOOM_LVL);
+            } catch (IOException e) {
+                Toast.makeText(this.getContext(), R.string.cannot_resolve_postcode, Toast.LENGTH_SHORT).show();
+                moveCameraToWales();
+            }
+        } else {
+            moveCameraToWales();
+        }
+
+    }
+
+    /**
+     * Moves the camera so it covers the entirety of wales.
+     */
+    private void moveCameraToWales() {
+        moveCameraToLocation(new LatLng(52.1307, -3.7837), WALES_ZOOM_LVL);
+
+    }
+
+    /**
+     * Moves the camera to a location
+     *
+     * @param l    the location
+     * @param zoom the zoom level
+     */
+    private void moveCameraToLocation(Location l, int zoom) {
+        moveCameraToLocation(new LatLng(l.getLatitude(), l.getLongitude()), zoom);
+    }
+
+    /**
+     * Moves camera to an address
+     *
+     * @param l    the address
+     * @param zoom the zoom level
+     */
+    private void moveCameraToLocation(Address l, int zoom) {
+        moveCameraToLocation(new LatLng(l.getLatitude(), l.getLongitude()), zoom);
+    }
+
+    /**
+     * Moves camera to a LatLng pair
+     *
+     * @param l    the LatLng pair
+     * @param zoom the zoom level
+     */
+    private void moveCameraToLocation(LatLng l, int zoom) {
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(l, zoom));
+
     }
 }
 
