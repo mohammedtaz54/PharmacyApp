ae0e38fd5c2816adbe426b3d61fce3b36a01e212
Hristo Dimitrov <dimitrovhd@cardiff.ac.uk>
2018-04-04 06:19:17 +0100

    Made ListView scrollable and translated

--- app/src/main/java/com/nsa/clientproject/welshpharmacy/PharmacyView.java
+++ app/src/main/java/com/nsa/clientproject/welshpharmacy/PharmacyView.java
@@ -3,6 +3,9 @@ package com.nsa.clientproject.welshpharmacy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
 import android.widget.TextView;
 import android.widget.ListView;
 
@@ -38,9 +41,10 @@ public class PharmacyView extends AppCompatActivity {
         //Change Text in name text field in single pharmacy view
         TextView singleName = (TextView) findViewById(R.id.single_name);
         singleName.setText(pharmacy.getName());
+        //Not necessary as we're showing all of them lower
         //Change Text in times text field in single pharmacy view
-        TextView singleTimes = (TextView) findViewById(R.id.single_times);
-        singleTimes.setText(getString(R.string.opening_closing_time, pharmacy.getOpenTime(), pharmacy.getCloseTime()));
+//        TextView singleTimes = (TextView) findViewById(R.id.single_times);
+//        singleTimes.setText(getString(R.string.opening_closing_time, pharmacy.getOpenTime(), pharmacy.getCloseTime()));
 
         //Change Text in location text field in single pharmacy view
         TextView singleLocation = (TextView) findViewById(R.id.single_location);
@@ -57,10 +61,10 @@ public class PharmacyView extends AppCompatActivity {
 
         services = new ArrayList<String>();
 
-        for(PharmacyServices pharmacyService: pharmacy.getServicesOffered()){
+        for (PharmacyServices pharmacyService : pharmacy.getServicesOffered()) {
             //Reference:https://stackoverflow.com/questions/4427608/android-getting-resource-id-from-string
             //Accessed 3 April 2018
-            services.add(getString(getResources().getIdentifier(pharmacyService.name(),"string",getPackageName())));
+            services.add(getString(getResources().getIdentifier(pharmacyService.name(), "string", getPackageName())));
         }
 
         // Create The Adapter with passing ArrayList as 3rd parameter
@@ -80,10 +84,10 @@ public class PharmacyView extends AppCompatActivity {
 
         services = new ArrayList<String>();
 
-        for(PharmacyServices pharmacyService: pharmacy.getServicesInWelsh()){
+        for (PharmacyServices pharmacyService : pharmacy.getServicesInWelsh()) {
             //Reference:https://stackoverflow.com/questions/4427608/android-getting-resource-id-from-string
             //Accessed 3 April 2018
-            services.add(getString(getResources().getIdentifier(pharmacyService.name(),"string",getPackageName())));
+            services.add(getString(getResources().getIdentifier(pharmacyService.name(), "string", getPackageName())));
         }
 
         // Create The Adapter with passing ArrayList as 3rd parameter
@@ -93,7 +97,7 @@ public class PharmacyView extends AppCompatActivity {
         serviceListWelsh.setAdapter(arrayAdapter);
 
         // register onClickListener to handle click events on each item
-        
+
         //Change items in services list view in single pharmacy view
         ArrayList<String> opening;
 
@@ -105,11 +109,19 @@ public class PharmacyView extends AppCompatActivity {
         ArrayList<String> timesArrayList = new ArrayList<>();
 
 
-        for(int i = 0; i < pharmacy.getOpeningTimes().values().size(); ++i){
-                String openingString = new ArrayList<>(pharmacy.getOpeningTimes().values()).get(i).toString();
-                String closingString = new ArrayList<>(pharmacy.getClosingTimes().values()).get(i).toString();
-                String dayString = new ArrayList<>(pharmacy.getClosingTimes().keySet()).get(i).toString();
-                timesArrayList.add(dayString +  ": " + openingString + " - " + closingString);
+        for (int i = 0; i < pharmacy.getOpeningTimes().values().size(); ++i) {
+            //Reference:https://stackoverflow.com/questions/4427608/android-getting-resource-id-from-string
+            //Accessed 3 April 2018
+
+            String openingString = new ArrayList<>(pharmacy.getOpeningTimes().values()).get(i).toString();
+            String closingString = new ArrayList<>(pharmacy.getClosingTimes().values()).get(i).toString();
+            String dayIdentifier = new ArrayList<>(pharmacy.getClosingTimes().keySet()).get(i).toString();
+            String dayString = getString(getResources().getIdentifier(dayIdentifier, "string", getPackageName()));
+            if (openingString.equals(closingString)) {
+                timesArrayList.add(dayString + ": " + getString(R.string.closed));
+            } else {
+                timesArrayList.add(dayString + ": " + openingString + " - " + closingString);
+            }
         }
 
         // Create The Adapter with passing ArrayList as 3rd parameter
@@ -117,10 +129,33 @@ public class PharmacyView extends AppCompatActivity {
                 new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, timesArrayList);
         // Set The Adapter
         serviceOpening.setAdapter(arrayAdapterTime);
-
+        setListViewHeightBasedOnChildren(serviceOpening);
+        setListViewHeightBasedOnChildren(serviceList);
+        setListViewHeightBasedOnChildren(serviceListWelsh);
         // register onClickListener to handle click events on each item
-        ;
 
 
+    }
+    //Reference: https://stackoverflow.com/questions/18367522/android-list-view-inside-a-scroll-view/20475821
+    //Accessed on 4 April 2018
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null)
+            return;
+
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.UNSPECIFIED);
+        int totalHeight = 0;
+        View view = null;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            view = listAdapter.getView(i, view, listView);
+            if (i == 0)
+                view.setLayoutParams(new ViewGroup.LayoutParams(desiredWidth, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+            view.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+            totalHeight += view.getMeasuredHeight();
+        }
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
     }
 }
