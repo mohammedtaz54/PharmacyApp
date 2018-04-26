af571abfc885dbf65156342d02abff759ad35d36
Hristo Dimitrov <hdimitrov@cardiff.ac.uk>
2018-04-26 09:38:48 +0100

    Admin app built

---
+++ pharmacyadmin/src/main/java/com/nsa/clientproject/pharmacyadmin/PharmacyList.java
@@ -0,0 +1,13 @@
+package com.nsa.clientproject.pharmacyadmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PharmacyList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pharmacy_list);
+    }
+}
