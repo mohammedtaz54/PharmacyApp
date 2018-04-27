601c4f61a5facc28f108e0068879d8efb737e589
Hristo Dimitrov <hdimitrov@cardiff.ac.uk>
2018-04-27 13:00:58 +0100

    Added an edit option for pharmacies

--- pharmacyadmin/src/main/java/com/nsa/clientproject/pharmacyadmin/AddPharmacyActivity.java
+++ pharmacyadmin/src/main/java/com/nsa/clientproject/pharmacyadmin/AddPharmacyActivity.java
@@ -1,5 +1,6 @@
 package com.nsa.clientproject.pharmacyadmin;
 
+import android.app.Activity;
 import android.content.IntentFilter;
 import android.location.Address;
 import android.location.Geocoder;
@@ -26,6 +27,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.google.gson.Gson;
+import com.nsa.clientproject.pharmacyadmin.models.Pharmacy;
 import com.nsa.clientproject.pharmacyadmin.models.PharmacyServices;
 
 import org.json.JSONArray;
@@ -33,6 +35,7 @@ import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.time.DayOfWeek;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -40,11 +43,40 @@ import java.util.List;
 import java.util.Map;
 
 public class AddPharmacyActivity extends AppCompatActivity implements View.OnClickListener {
-
+    /**
+     * Stores if the user is in edit mode or not.
+     */
+    private boolean editMode = false;
+    /**
+     * Stores the pharmacy's id
+     */
+    private int pharmacyId;
+    /**
+     * Stores the pharmacy we're editing
+     */
+    private Pharmacy pharmacy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pharmacy_add_edit);
+        if(getIntent().hasExtra("pharmacy")){
+            editMode = true;
+            pharmacy = (Pharmacy) getIntent().getSerializableExtra("pharmacy");
+            pharmacyId = getIntent().getIntExtra("pharmacyId",0);
+            ((EditText) findViewById(R.id.pharmacy_email)).setText(pharmacy.getEmail());
+            ((EditText) findViewById(R.id.pharmacy_phone)).setText(pharmacy.getPhone());
+            ((EditText) findViewById(R.id.pharmacy_website)).setText(pharmacy.getWebsite());
+            ((EditText) findViewById(R.id.pharmacy_address)).setText(pharmacy.getLocation());
+            ((EditText) findViewById(R.id.pharmacy_postcode)).setText(pharmacy.getPostcode());
+            ((EditText) findViewById(R.id.pharmacy_name)).setText(pharmacy.getName());
+
+
+
+
+
+        }
+
+
         LinearLayoutCompat weekdays = findViewById(R.id.week_day_checkboxes);
         for (DayOfWeek d : DayOfWeek.values()) {
             AppCompatCheckBox checkbox = new AppCompatCheckBox(this);
@@ -78,13 +110,26 @@ public class AddPharmacyActivity extends AppCompatActivity implements View.OnCli
             line.addView(checkbox);
             line.addView(startTime);
             line.addView(finishTime);
-            if (d != DayOfWeek.SATURDAY && d != DayOfWeek.SUNDAY) {
-                checkbox.setChecked(true);
-                startTime.setText(getString(R.string.R_id_nine_thirty));
-                finishTime.setText(R.string.R_id_five_pm);
-            } else {
-                startTime.setText(R.string.zero_zero_time);
-                finishTime.setText(R.string.zero_zero_time);
+            if(!editMode) {
+                if (d != DayOfWeek.SATURDAY && d != DayOfWeek.SUNDAY) {
+                    checkbox.setChecked(true);
+                    startTime.setText(getString(R.string.R_id_nine_thirty));
+                    finishTime.setText(R.string.R_id_five_pm);
+                } else {
+                    startTime.setText(R.string.zero_zero_time);
+                    finishTime.setText(R.string.zero_zero_time);
+                }
+            }
+            else{
+                if(pharmacy.getOpeningTimes().get(d)==pharmacy.getClosingTimes().get(d)){
+                    startTime.setText(R.string.zero_zero_time);
+                    finishTime.setText(R.string.zero_zero_time);
+                }
+                else{
+                    startTime.setText(pharmacy.getOpeningTimes().get(d).toString());
+                    finishTime.setText(pharmacy.getClosingTimes().get(d).toString());
+                    checkbox.setChecked(true);
+                }
             }
             weekdays.addView(line);
         }
@@ -95,9 +140,19 @@ public class AddPharmacyActivity extends AppCompatActivity implements View.OnCli
             AppCompatCheckBox currentCheckbox = new AppCompatCheckBox(this);
             currentCheckbox.setTag(service.name());
             currentCheckbox.setText(getResources().getIdentifier(service.name(), "string", getPackageName()));
+
+
             AppCompatCheckBox currentCheckboxWelsh = new AppCompatCheckBox(this);
             currentCheckboxWelsh.setTag(service.name());
             currentCheckboxWelsh.setText(getResources().getIdentifier(service.name(), "string", getPackageName()));
+            if(editMode) {
+                if (pharmacy.getServicesOffered().contains(service)) {
+                    currentCheckbox.setChecked(true);
+                }
+                if(pharmacy.getServicesInWelsh().contains(service)){
+                    currentCheckboxWelsh.setChecked(true);
+                }
+            }
             services.addView(currentCheckbox);
             servicesWelsh.addView(currentCheckboxWelsh);
         }
@@ -114,7 +169,7 @@ public class AddPharmacyActivity extends AppCompatActivity implements View.OnCli
             final String website = ((EditText) findViewById(R.id.pharmacy_website)).getText().toString();
             final String phone = ((EditText) findViewById(R.id.pharmacy_phone)).getText().toString();
             final String email = ((EditText) findViewById(R.id.pharmacy_email)).getText().toString();
-            List<Integer[]> openingClosingTimes = new ArrayList<>();
+            final List<Integer[]> openingClosingTimes = new ArrayList<>();
 
 
             //todo: somehow make this more clear, the parsing of weekdays is a mess.
@@ -191,8 +246,11 @@ public class AddPharmacyActivity extends AppCompatActivity implements View.OnCli
                     openingClosingTimes.add(currentOpeningTimes);
                 }
             }
+
+
+            //Prepare services
             LinearLayoutCompat services = findViewById(R.id.services);
-            List<String> pharmacyServices = new ArrayList<>();
+            final List<String> pharmacyServices = new ArrayList<>();
             for(int i=0;i<services.getChildCount();i++){
                 CheckBox c = (CheckBox) services.getChildAt(i);
                 if(c.isChecked()){
@@ -200,53 +258,79 @@ public class AddPharmacyActivity extends AppCompatActivity implements View.OnCli
                 }
             }
             LinearLayoutCompat servicesWelsh = findViewById(R.id.services_welsh);
-            List<String> pharmacyServicesWelsh = new ArrayList<>();
+            final List<String> pharmacyServicesWelsh = new ArrayList<>();
             for(int i=0;i<servicesWelsh.getChildCount();i++){
                 CheckBox c = (CheckBox) servicesWelsh.getChildAt(i);
                 if(c.isChecked()){
                     pharmacyServicesWelsh.add(c.getTag().toString());
                 }
             }
-            Gson gson = new Gson();
-            final String openingClosingTimesField = gson.toJson(openingClosingTimes);
-            final String servicesField = gson.toJson(pharmacyServices);
-            final String servicesWelshField = gson.toJson(pharmacyServicesWelsh);
-            final Address finalAddressParsed = addressParsed;
-            RequestQueue requestQueue = RequestQueueSingleton.getInstance(this).getRequestQueue();
-            StringRequest jsonObjectRequest = new StringRequest(Request.Method.POST, "https://hdimitrov.pythonanywhere.com/pharmacies", new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    Toast.makeText(AddPharmacyActivity.this, R.string.successfully_added_pharmacy, Toast.LENGTH_SHORT).show();
 
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(AddPharmacyActivity.this, R.string.no_connection, Toast.LENGTH_SHORT).show();
 
-                    Log.d("ERROR",error.toString());
-                }
-            }){
-                @Override
-                protected Map<String, String> getParams() {
-                    HashMap<String,String> params = new HashMap<>();
-                    params.put("name",name);
-                    params.put("email",email);
-                    params.put("address",address);
-                    params.put("phone",phone);
-                    params.put("website",website);
-                    params.put("postcode",postcode);
-                    params.put("openingClosingTimes",openingClosingTimesField);
-                    params.put("services",servicesField);
-                    params.put("servicesInWelsh",servicesWelshField);
-                    params.put("lat", Double.toString(finalAddressParsed.getLatitude()));
-                    params.put("lng", Double.toString(finalAddressParsed.getLongitude()));
-                    Log.d("params",openingClosingTimesField);
-                    return params;
-                }
-            };
-            requestQueue.add(jsonObjectRequest);
+            //Actually send requests.
+            if(editMode){
+                final Address finalAddressParsed = addressParsed;
+                StringRequest deletePharmacyRequest = new StringRequest(Request.Method.DELETE, "http://hdimitrov.pythonanywhere.com/deletePharmacy/" + Integer.toString(pharmacyId), new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        doAddRequest(name, postcode, address, website, phone, email, openingClosingTimes, finalAddressParsed, pharmacyServices, pharmacyServicesWelsh);
+                    }
+                },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(getApplicationContext(), R.string.no_connection, Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+                RequestQueueSingleton.getInstance(this).getRequestQueue().add(deletePharmacyRequest);
+            }
+            else{
+                doAddRequest(name, postcode, address, website, phone, email, openingClosingTimes, addressParsed, pharmacyServices, pharmacyServicesWelsh);
+            }
         }
+    }
+
+    private void doAddRequest(final String name, final String postcode, final String address, final String website, final String phone, final String email, List<Integer[]> openingClosingTimes, Address addressParsed, List<String> pharmacyServices, List<String> pharmacyServicesWelsh) {
+        Gson gson = new Gson();
+        final String openingClosingTimesField = gson.toJson(openingClosingTimes);
+        final String servicesField = gson.toJson(pharmacyServices);
+        final String servicesWelshField = gson.toJson(pharmacyServicesWelsh);
+        final Address finalAddressParsed = addressParsed;
+        RequestQueue requestQueue = RequestQueueSingleton.getInstance(this).getRequestQueue();
+        StringRequest jsonObjectRequest = new StringRequest(Request.Method.POST, "https://hdimitrov.pythonanywhere.com/pharmacies", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(AddPharmacyActivity.this, R.string.successfully_added_pharmacy, Toast.LENGTH_SHORT).show();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(AddPharmacyActivity.this, R.string.no_connection, Toast.LENGTH_SHORT).show();
+
+                Log.d("ERROR",error.toString());
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() {
+                HashMap<String,String> params = new HashMap<>();
+                params.put("name",name);
+                params.put("email",email);
+                params.put("address",address);
+                params.put("phone",phone);
+                params.put("website",website);
+                params.put("postcode",postcode);
+                params.put("openingClosingTimes",openingClosingTimesField);
+                params.put("services",servicesField);
+                params.put("servicesInWelsh",servicesWelshField);
+                params.put("lat", Double.toString(finalAddressParsed.getLatitude()));
+                params.put("lng", Double.toString(finalAddressParsed.getLongitude()));
+                Log.d("params",openingClosingTimesField);
+                return params;
+            }
+        };
+        requestQueue.add(jsonObjectRequest);
     }
 
     /**
